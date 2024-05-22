6fdb4792c692dda282cda8d8e9b6608889bb5e70
isavmv <isadora.varoni@escola.pr.gov.br>
2024-05-22 09:36:39 -0300

    18

---
+++ main.js
@@ -0,0 +1,151 @@
+document.querySelector('.parametro-senha__botao');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+numeroSenha.textContent = 5;
+const botoes = document.querySelector('.parametro-senha__botao');
+const botoes = document.querySelector('.parametro-senha__botao');
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+console.log(botoes);
+function diminuiTamanho(){
+}
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+    let tamanhoSenha = 12;
+    senha.textContent = tamanhoSenha;
+    function diminuiTamanho(){
+        tamanhoSenha = tamanhoSenha - 1;
+        }
+        function diminuiTamanho(){
+            tamanhoSenha = tamanhoSenha - 1;
+            numeroSenha.textContent = tamanhoSenha
+            }
+            botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function aumentaTamanho() {
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha;
+    }
+    function diminuiTamanho() {
+        if (tamanhoSenha > 1)
+        tamanhoSenha = tamanhoSenha - 1;
+        numeroSenha.textContent = tamanhoSenha;
+        }
+        function aumentaTamanho() {
+            if (tamanhoSenha < 20) {
+            tamanhoSenha = tamanhoSenha + 1;
+            }
+            numeroSenha.textContent = tamanhoSenha;
+            }
+            // código omitido
+const campoSenha = document.querySelector('#campo-senha');
+// código omitido
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+// código omitido
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+campoSenha.textContent = letrasMaiusculas;
+campoSenha.value = letrasMaiusculas;
+// código omitido
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+function geraSenha(){
+}
+campoSenha.value = letrasMaiusculas;
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+function geraSenha(){
+let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+console.log(numeroAleatorio);
+}
+campoSenha.value = letrasMaiusculas;
+function geraSenha()
+{
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);}
+    // código omitido
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+function geraSenha(){
+let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+numeroAleatorio = Math.floor(numeroAleatorio);
+console.log(letrasMaiusculas[numeroAleatorio]);
+}
+campoSenha.value = letrasMaiusculas;
+// código omitido
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+function geraSenha(){
+for (let i = 0; i < tamanhoSenha;i++) {
+let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+numeroAleatorio = Math.floor(numeroAleatorio);
+console.log(letrasMaiusculas[numeroAleatorio]);
+}
+}
+campoSenha.value = letrasMaiusculas;
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+    // tamanhoSenha = tamanhoSenha-1;
+    tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+    }
+    function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+    // tamanhoSenha = tamanhoSenha+1;
+    tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+    }
+    // código omitido
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++) {
+    let numeroAleatorio = Math.random()*letrasMaiusculas
+    
+    length;
+    
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    console.log(letrasMaiusculas[numeroAleatorio]);
+    
+    }
+    }
+    campoSenha.value = letrasMaiusculas;
+    function geraSenha(){
+        let senha = '';
+        for (let i = 0; i < tamanhoSenha;i++){
+        
+        let numeroAleatorio = Math.random()*letrasMaiusculaslength;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+        console.log(letrasMaiusculas[numeroAleatorio]);
+        
+        }
+        campoSenha.value = letrasMaiusculas;
+        }
+        function geraSenha(){
+            let senha = '';
+            for (let i = 0; i < tamanhoSenha;i++){
+            
+            let numeroAleatorio = Math.random()*letrasMaiusculas
+            
+            length;
+            
+            numeroAleatorio = Math.floor(numeroAleatorio);
+            senha = senha + letrasMaiusculas[numeroAleatorio];
+            
+            }
+            campoSenha.value = senha;
+            }
+        
