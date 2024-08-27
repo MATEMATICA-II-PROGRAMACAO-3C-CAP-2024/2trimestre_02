8d027f96fa5fc4369486e585835b30543ddab110
isavmv <isadora.varoni@escola.pr.gov.br>
2024-08-27 09:13:30 -0300

    teste 8

--- main.js
+++ main.js
@@ -148,4 +148,49 @@ function geraSenha(){
             }
             campoSenha.value = senha;
             }
-        
+            document.querySelector('.parametro-senha__botao');
+            const numeroSenha = document.querySelector('.parametro-senha__texto');
+            const numeroSenha = document.querySelector('.parametro-senha__texto');
+numeroSenha.textContent = 5;
+const botoes = document.querySelector('.parametro-senha__botao');
+const botoes = document.querySelector('.parametro-senha__botao');
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+console.log(botoes);
+function diminuiTamanho(){
+}
+const numeroSenha = document.querySelector('parametro-senha__texto');
+    let tamanhoSenha = 12;
+    senha.textContent = tamanhoSenha;
+    function diminuiTamanho(){
+        tamanhoSenha = tamanhoSenha - 1;
+        }
+        function diminuiTamanho(){
+            tamanhoSenha = tamanhoSenha - 1;
+            numeroSenha.textContent = tamanhoSenha
+            }
+            botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function aumentaTamanho() {
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha;
+    }
+    function diminuiTamanho() {
+        if (tamanhoSenha > 1)
+        tamanhoSenha = tamanhoSenha - 1;
+        numeroSenha.textContent = tamanhoSenha;
+        }
+        function aumentaTamanho() {
+            if (tamanhoSenha < 20) {
+            tamanhoSenha = tamanhoSenha + 1;
+            }
+            numeroSenha.textContent = tamanhoSenha;
+            }
+            // código omitido
+const campoSenha = document.querySelector(#campo-senha);
